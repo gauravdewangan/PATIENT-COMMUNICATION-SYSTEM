da780445f94895afe83e12f6032ad5e58ce7318b
gauravdewangan <37238897+gauravdewangan@users.noreply.github.com>
2018-03-18 11:14:14 +0530

    Update touch.ino

--- touch.ino
+++ touch.ino
@@ -1,18 +1,18 @@
-int x,y = 0;
-int  a,b,c=0;
+int x,y ;
+int  a,b,c;
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(6, 8, 10, 11, 12, 13);
 void setup()
 {
-
+pinMode(5,OUTPUT);
 lcd.begin(16, 2);
-
+Serial.begin(9600);
 
 lcd.clear();
-      lcd.print("IMMEDIATE ACTION");
+      lcd.print("PATIENT ALERT");
       lcd.setCursor(0, 1);
       lcd.print("SYSTEM");
-      delay(1000);
+      delay(100);
 }
 int readX() // returns the value of the touch screen's X-axis
 {
@@ -24,7 +24,8 @@ pinMode(17, OUTPUT);   // A3
 digitalWrite(15, LOW); // set A1 to GND
 digitalWrite(17, HIGH);  // set A3 as 5V
 delay(15); // short delay is required to give the analog pins time to adjust to their new roles
-xr=analogRead(0); //  return xr;
+xr=analogRead(0); // 
+return xr;
 }
 int readY() // returns the value of the touch screen's Y-axis
 {
@@ -37,68 +38,98 @@ digitalWrite(14, LOW); // set A0 to GND
 digitalWrite(16, HIGH);  // set A2 as 5V
 delay(15); // short delay is required to give the analog pins time to adjust to their new roles
 yr=analogRead(1); //
-return yr;
+ return yr;
 }
+
+
 void loop()
 {
 
 x=readX();
 
 y=readY();
- if ( x>520 && x<880 && y>550 && y <910)
+Serial.print(y);
+delay(1000);
+Serial.print(x);
+delay(100);
+
+
+ if ( y>550 && y<900 && x>550 && x <900)
   {
     a=1;
     //lcd.clear();
-    //lcd.print("TABLE NO:1");
+    //lcd.print("BED NO:1");
     //lcd.setCursor(0, 1);
-    //lcd.print("DOSA");
-    delay(1000);               
+    //lcd.print("EMERGENCY");
+    delay(1000); 
+                  
  
-  }  else
-    if (x>130 && x<500 && y>550 && y <900)
+  } 
+  else if (y>300 && y<550 &&  x <550)
     {
       b=1;
      //lcd.clear();
-    //lcd.print("TABLE NO:1");
+    //lcd.print("BED NO:1");
     //lcd.setCursor(0, 1);
-    //lcd.print("TEA");
+    //lcd.print("WASHROOM");
     delay(1000);   
     
-  } else   if (x>520 && x<879 && y>136 && y <527)
+  } else if(y>550 && y<900 && x>0 && x<550)
     {
       c=1;
     //lcd.clear();
-  //lcd.print("TABLE NO:1");
+  //lcd.print("BED NO:1");
    //lcd.setCursor(0, 1);
-//lcd.print("COFFEE");
+//lcd.print("WATER");
     delay(1000);   
       
-    }  else   if (x>130 && x<500 && y>110 && y <532)
+    }  
+
+else if (y>0 && y<550 && x>550 && x <900)
     {
       
       if (a==1)
       {
       lcd.clear();
-      lcd.print("TABLE NO:1");
+      lcd.print("BED NO:1");
       lcd.setCursor(0, 1);
-      lcd.print("IDLI");
+      lcd.print("EMERGENCY");
       delay(1000);
-      }   else  if (b==1)
+      digitalWrite(5,HIGH);
+      delay(1000);
+       digitalWrite(5,LOW);
+       delay(5000);
+       a=0;
+      }   
+      
+      else if (b==1)
       {
       lcd.clear();
-      lcd.print("TABLE NO:1");
+      lcd.print("BED NO:1");
       lcd.setCursor(0, 1);
-      lcd.print("TEA");
+      lcd.print("WASHROOM");
       delay(1000);
-      } else  if (c==1)
-      {
-      lcd.clear();
-      lcd.print("TABLE NO:1");
-      lcd.setCursor(0, 1);
-      lcd.print("DOSA");
+      digitalWrite(5,HIGH);
       delay(1000);
+      digitalWrite(5,LOW);
+      delay(5000);
+       b=0;
       } 
+      
+      else
+      {
+      lcd.clear();
+      lcd.print("BED NO:1");
+      lcd.setCursor(0, 1);
+      lcd.print("WATER REQUIRED");
+      delay(1000);
+       digitalWrite(5,HIGH);
+       delay(1000);
+       digitalWrite(5,LOW);
+       delay(5000);
+       c=0;
+      } 
+      
   }
-
 delay (200);
 }
